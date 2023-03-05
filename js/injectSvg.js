b6208d8b99aa634999512042c3b64cb1e0f08a3c
Henrik Rodrigues Marinho <henrikrodriguesjob@gmail.com>
2023-03-05 13:50:13 -0300

    inject svg and structure changed

---
+++ js/injectSvg.js
@@ -0,0 +1,15 @@
+const fetchSvg = (image) => {
+   fetch(image.src)
+      .then((response) => response.text())
+      .then((data) => {
+         const span = document.createElement("span");
+         span.innerHTML = data;
+         let inlineSvg = span.querySelector("svg");
+         inlineSvg.setAttribute("id", "instagramIcon");
+         if(image.src !== "http://127.0.0.1:5500/assets/instagram.svg") {
+            inlineSvg.setAttribute("id", "linkedinIcon");
+         };
+         image.parentNode.replaceChild(inlineSvg, image);
+         return true;
+      });
+};
